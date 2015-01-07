91dce8256bed0db59b6eb211b303e7c4e8adeb6e
Jeff Carnegie <jeff.carnegie@gmail.com>
2015-01-07 12:17:00 -0800

    Fixed issue with layouts and context

--- client/layouts/app.jsx
+++ client/layouts/app.jsx
@@ -1,9 +1,23 @@
 /**
  * @jsx React.DOM
  */
-var React = require("react");
+var React    = require("react");
+var isoreact = require("isoreact");
 
 module.exports = React.createClass({
+    mixins: [isoreact.mixin],
+
+    childContextTypes: {
+        reqId: React.PropTypes.string,
+        state: React.PropTypes.object,
+        params: React.PropTypes.object,
+        query: React.PropTypes.object
+    },
+
+    getChildContext: function() {
+        return this.props.context;
+    },
+
     render: function() {
         return (
             <div>
--- client/lib/router.js
+++ client/lib/router.js
@@ -102,24 +102,19 @@ var reactPageAction = r.curry(function(containerSelector, layout, component) {
  * @return {[type]}                   [description]
  */
 var reactPageActionWithState = r.curry(function(state, containerSelector, layout, component) {
-    return function(route, params) { 
+    return function(route, params) {
         var context = {
             params: params,
             query: querystring.parse(window.location.search),
             state: state || {}
         };
 
-        var element = react.createFactory(layout);
-        // does element need context passed in? i.e. element(context)?
-        // var conextualElement = react.withContext(context, function() { return element(context); });
-        var props = { body: component };
-        var conextualElement = react.withContext(context, function() { return element(props); });
-        var domElement = document.querySelector(containerSelector);
-
-        console.log("rendering react component to: ");
-        console.log(domElement);
-
-        react.render(conextualElement, domElement);
+        react.withContext(context, function() {
+            var element = react.createFactory(layout);
+            var props = { body: react.createFactory(component), context: context };
+            var domElement = document.querySelector(containerSelector);
+            react.render(element(props), domElement);
+        });
     }
 });
 
